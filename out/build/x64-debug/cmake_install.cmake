322877ee1bb236602e64834d0130823cd866559b
wasim15185 <wasimakram15185@gmail.com>
2022-08-08 10:35:27 +0530

    Refactor Code

--- out/build/x64-debug/cmake_install.cmake
+++ out/build/x64-debug/cmake_install.cmake
@@ -34,7 +34,7 @@ endif()
 
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   # Include the install script for each subdirectory.
-  include("C:/Users/WASIM/Desktop/Zink-Lang/out/build/x64-debug/src/cmake_install.cmake")
+  include("C:/Users/WASIM/Desktop/Zink-Lang/out/build/x64-Debug/src/cmake_install.cmake")
 
 endif()
 
@@ -46,5 +46,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "C:/Users/WASIM/Desktop/Zink-Lang/out/build/x64-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "C:/Users/WASIM/Desktop/Zink-Lang/out/build/x64-Debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
---
+++ src/Zink.h
@@ -0,0 +1,3 @@
+#pragma once  // "pragma once" --> Specifies that the compiler includes the header file only once, when compiling a source code file.
+
+#include <iostream>
