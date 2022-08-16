a40172ff48944d999f1127e76140b63c4d55a1dd
wasim15185 <wasimakram15185@gmail.com>
2022-08-16 23:31:58 +0530

    Refactor Token.hpp & Token.cpp

--- frontend/source/Token.cpp
+++ frontend/source/Token.cpp
@@ -1,1 +1,7 @@
 #include"Token.hpp"
+
+using namespace zink;
+
+frontend::Token::Token(const TokenType* type, std::string lexeme, std::string litaralStr, int line) { // constructor
+
+}
--- frontend/source/Token.hpp
+++ frontend/source/Token.hpp
@@ -1,18 +1,25 @@
 #pragma once 
 #include<iostream>
+#include "TokenType.hpp"
 namespace zink {
 
 	namespace frontend {
 
 		class Token {
 
+		public:
+			Token(const TokenType* type, std::string lexeme, std::string litaralStr, int line);
 
-  
+
+
+		};
 
 
 
-		 };
+		struct Litaral
+		{
 
+		};
 
 	}
 
